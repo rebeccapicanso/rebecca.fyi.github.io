bb15a1ada41a44e47937ddc64a652c62600bb7cd
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-02-25 20:32:45 -0500

    Update and rename index.php to robot.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-   $robot = $_POST['name'];
-   if (isset($_POST['submit'])){
-       print "<h1>$robot</h1>";
-   }
-?>
---
+++ robot.php
@@ -0,0 +1,5 @@
+<?php
+   if (isset($_POST['submit'])){
+         header("Location: main.html");
+   }
+?>
